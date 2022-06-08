4364fa92027526e05e3d24d74faa5243b5d51284
PC <ITSuKai@163.com>
2022-06-08 10:47:44 +0800

    webscoket与定时任务

---
+++ src/main/java/com/example/bigScreen/config/websocket/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.bigScreen.config.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author: SuKai
+ * @date: 2022/6/8
+ * @time: 9:58
+ * @description:
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+
+        return new ServerEndpointExporter();
+    }
+}
